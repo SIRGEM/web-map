aedbf6ba85e5478249a941237b4e3224a77d2ccc
Martin Nievas <martin.nievas.ar@gmail.com>
2024-08-25 00:58:48 -0300

    Agrega ejemplo con visualización de marcadores

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
 	import { Map, TileLayer, Marker, Popup } from 'sveaflet';
+
+    let markers_position = [
+        [-31.44146561060643, -64.19316654865654],
+        [-31.43826082308847, -64.1933846882900],
+        [-31.43420345093465, -64.18607029718518],
+    ];
 </script>
 
 <div style="width:100%;height:800px;">
@@ -10,6 +16,16 @@
 		}}
 	>
 		<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
-		<Marker latLng={[-31.44146561060643, -64.19316654865654]} />
+        {#each markers_position as position}
+            <Marker latLng={position}>
+                <Popup>
+                    <div>
+                        <h1>Marker</h1>
+                        <p>Lat: {position[0]}</p>
+                        <p>Lng: {position[1]}</p>
+                    </div>
+                </Popup>
+            </Marker>
+        {/each}
 	</Map>
 </div>
